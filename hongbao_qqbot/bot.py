7dfc3bf4ce78c62b6fdc0b3ed3b2929818c734bd
leetao <leetao94cn@gmail.com>
2018-05-15 22:47:17 +0800

    add bot isAuth and loadAuthDict func

--- hongbao_qqbot/bot.py
+++ hongbao_qqbot/bot.py
@@ -1,11 +1,12 @@
 # -*- coding:utf-8 -*-
 
 from qqbot import QQBotSlot as qqbotslot, RunBot
-
+import json
+import os
 
 class Bot:
 
-    __slots__ = ['chat_enabled', 'share_enabled', 'remember_enabled', 'account', 'password', 'bot_name', 'need_train', 'train_data']
+    __slots__ = ['chat_enabled', 'share_enabled', 'remember_enabled', 'account', 'password', 'bot_name', 'need_train', 'train_data', 'auth_path']
 
     def __init__(self, **kwargs):
         self.chat_enabled = kwargs.get('chat_enabled', False)
@@ -16,7 +17,7 @@ class Bot:
         self.bot_name = kwargs.get('bot_name', 'mtdhb')
         self.need_train = kwargs.get('need_train', False)
         self.train_data = kwargs.get('train_data', '*')
-
+        self.auth_path = 'store.json'
 
     @qqbotslot
     def onQQMessage(self, bot, contact, member, content):
@@ -28,6 +29,23 @@ class Bot:
                 pass
             else:
                 pass
+
+    def isAuth(self, qq):
+        user_auth_dict = self.loadAuthDict()
+        if user_auth_dict is False:
+            return False
+        else:
+            auth = user_auth_dict.get(qq,False)
+        return auth
+
+    def loadAuthDict(self):
+        if os.path.exists(self.auth_path):
+            with open(self.auth_path,'r') as f:
+                user_auth_dict =  json.load(f)
+            return user_auth_dict
+        else:
+            return False
+
 
     def __call__(self): 
         RunBot()
